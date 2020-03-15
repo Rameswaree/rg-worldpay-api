c26e650321b465f78e7a62d02e863d0f1daff11b
Rameswaree <43037382+Rameswaree@users.noreply.github.com>
2020-03-15 09:15:57 +0000

    Create WorldPayApplication.java

---
+++ src/main/java/com/worldpay/WorldPayApplication.java
@@ -0,0 +1,12 @@
+package com.worldpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldPayApplication{
+
+      public static void main(String[] args){
+            SpringApplication.run(WorlPayApplication.class,args);
+      }
+}
